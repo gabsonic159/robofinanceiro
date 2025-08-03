4e1a77d8b9b2d86b5098c1cb7c5571551ced09e4
Gabriel de Camargo Sega <gcamargo217@gmail.com>
2025-08-03 19:14:48 -0300

    Versão mudanças

--- gastos.py
+++ gastos.py
@@ -22,7 +22,7 @@ from telegram.ext import (
     ContextTypes,
 )
 
-# --- Configuração da Base de Dados ---
+# --- Configuração da Base de Dados --- testetes 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 # ### MUDANÇA ###: Removida a lógica do disco do Render, pois não está disponível no plano gratuito.
 # O banco de dados será "efêmero", ou seja, reiniciado com o servidor.
